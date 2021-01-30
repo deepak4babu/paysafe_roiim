6090b51778ed5eb9c562659c3318a88c9458f526
Deepak <40018681+deepak4babu@users.noreply.github.com>
2021-01-30 22:03:31 +0530

    Delete paysafe_config.py

--- paysafe/utils/paysafe_config.py
+++
@@ -1,6 +0,0 @@
-PRIVATE_KEY_USERNAME = <private-key>
-PRIVATE_KEY_VALUE = <private-key-value>
-PRIVATE_KEY_BASE64 = <private-key-value-base64>
-PUBLIC_KEY_USERNAME = <public-key>
-PUBLIC_KEY_VALUE = <public-key-value>
-PUBLIC_KEY_BASE64 = <public-key-value-base64>
